5c01941749ef2cf0026a04e72df5dd370ebf5aed
Alice Arnold <alicearnold@Alices-MBP.attlocal.net>
2021-04-29 18:26:23 -0500

    update

--- static/app.js
+++ static/app.js
@@ -6,6 +6,9 @@
 //     var team_name = data.map(d => d.team_name)
 //     console.log(team_name)
 
+
+// **************BOX PLOT FOR BATTING AVERAGE**********************************************************
+
     var trace1 = {
         y: batting_avg.ba_day,
         name: "DAY GAMES",
@@ -14,7 +17,7 @@
       };
       
       var trace2 = {
-        y: batting_avg.ba-night,
+        y: batting_avg.ba_night,
         name: "NIGHT GAMES",
         type: "box",
         boxpoints: "all"
@@ -29,5 +32,68 @@
       
       // Plot the chart to a div tag with id "plot"
       Plotly.newPlot("plot", data, layout);
+
+
+    // *************  BOXPLOT FOR EARN RUN AVERAGES *******************************************************
       
+
+      var trace3 = {
+        y: earn_run_avg.era_day,
+        name: "DAY GAMES",
+        type: "box",
+        boxpoints: "all"
+      };
+      
+      var trace4 = {
+        y: earn_run_avg.era_night,
+        name: "NIGHT GAMES",
+        type: "box",
+        boxpoints: "all"
+      };
+      
+      var data = [trace3, trace4];
+      
+      var layout = {
+        title: "EARN RUN AVERAGES DURING NIGHT AND DAY GAMES",
+        yaxis: { title: "Earn Run Average"}
+      };
+      
+      // Plot the chart to a div tag with id "plot"
+      Plotly.newPlot("plot", data, layout);
+
+    //    ******************** Part 5  PIE CHAT FOR DAY GAMES ***************************************************
+
+    
+
+var trace5 = {
+    labels: ["Batting Average", "Earn Run Average"],
+    values: [],
+    type: 'pie'
+  };
+      
+  var data = [];
+
+var layout = {
+  title: "Pie Chart for Day Games",
+};
+
+Plotly.newPlot("plot", data, layout);
+
+
+// ******************** Part 6  PIE CHAT FOR DAY GAMES ***********************************************************
+
+var trace6 = {
+    labels: ["Batting Average", "Earn Run Average"],
+    values: [  ],
+    type: 'pie'
+  };
+      
+  var data = [];
+
+var layout = {
+  title: "Pie  Chart for Night Games",
+};
+
+Plotly.newPlot("plot", data, layout);
+
 
